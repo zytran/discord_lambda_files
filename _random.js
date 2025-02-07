998f9a91535e74d8f482b3fa0aea0abb4010a7ce
Zy Tran <zrt5062@psu.edu>
2025-02-07 12:47:14 -0500

    AWS Lambda files for a Discord Bot

---
+++ _random.js
@@ -0,0 +1,25 @@
+//testCommandHandler.js
+
+module.exports = function (body) {
+  // Extract options from the interaction
+  const gameOption = body.data.options.find((option) => option.name === 'game');
+  const randomGame = gameOption.value;
+  
+  const eightBallResponses = randomGame.split(",");
+  if (true) {
+    //const greetingMessage = gameOption ? gameOption.value : 'xDDD.';
+    //const randomResponse = eightBallResponses[Math.floor(Math.random() * eightBallResponses.length)];
+    const randomResponse = "hello"
+    // Customize the response message based on the options
+    const responseMessage = `<@${body.member.user.id}> Asked: ${randomGame}\n**8Ball Answered:** ${randomResponse} `;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        type: 4,
+        data: { content: responseMessage },
+      }),
+    };
+  }
+};
+
---
+++ _testCommand.js
@@ -0,0 +1,50 @@
+module.exports = function (body) {
+  const slotNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]; // Change to emojis later
+  
+  const fs = require("fs");
+  const path = "./money.json";
+
+  let num1 = slotNumbers[Math.floor(Math.random() * slotNumbers.length)];
+  let num2 = slotNumbers[Math.floor(Math.random() * slotNumbers.length)];
+  let num3 = slotNumbers[Math.floor(Math.random() * slotNumbers.length)];
+
+  // Test win condition
+  //num1 = "7";
+  //num2 = "7";
+  //num3 = "7";
+
+  let slotSelection = `→ ${num1} ${num2} ${num3}`;
+
+  // Get user's ID
+  const userId = body.member.user.id;
+
+  // Check if user won
+  if (num1 === num2 && num2 === num3) {
+    slotSelection += `\n<@${userId}> Won Big!!!`;
+
+    // Read and update money.json
+    let userMoney = JSON.parse(fs.readFileSync(path, "utf-8"));
+
+    
+    if (!userMoney[userId]) {
+      userMoney[userId] = 0; // Initialize if user doesn't exist
+    }
+    
+    userMoney[userId] += 100; // Add winnings (change amount as needed)
+
+    // Save updated money.json
+    //fs.writeFileSync("./money.json", JSON.stringify(userMoney, null, 2), "utf-8");
+
+  }
+
+  slotSelection+=`\n\n**This is a test command. No values are saved and will be added at a later date**`
+
+  // Return response to Discord
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      type: 4,
+      data: { content: slotSelection },
+    }),
+  };
+};
