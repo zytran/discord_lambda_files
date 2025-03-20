808e3930b49342eee4dc6c3d3dc5639dbb56a05e
Zy Tran <zrt5062@psu.edu>
2025-03-20 17:31:28 -0400

    Started the catch command for pokemon

--- _commands.js
+++ _commands.js
@@ -1,20 +1,36 @@
-// This fuction is a way for the both me and the user to know what commands are available
+const { EmbedBuilder } = require('discord.js');
 
-module.exports = async(body)=> {
-    
-    const userID = body.member.user.id;
+module.exports = async(body)=>{
+    const exampleEmbed = new EmbedBuilder()
+    .setColor(0xFFC5D3)
+    .setTitle('Commands Test')
+    .setURL('https://Yoshingo.org/')
+    .setAuthor({ name: 'Yoshingo', iconURL: 'https://i.imgur.com/IlcxbcC.jpeg', url: 'https://discord.js.org' })
+    .setDescription('fortnite balls')
+    .setThumbnail('https://i.imgur.com/PeDQKLE.jpeg')
+    .addFields(
+        { name: 'Regular field title', value: 'Some value here' },
+        { name: '\u200B', value: '\u200B' },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+        {name: 'test field here', value:"idk whats valueable", inline: true},
+    )
+    //.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+    .setImage('https://i.imgur.com/IlcxbcC.jpeg')
+    .setTimestamp()
+    .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
-    let userMessage = `# Hello <@${userID}> this is a list of all the commands currently available, with all the parameters available:\n\n`;
-    userMessage+= `\`/hello\`: Greets the user with a message`
-    userMessage+= `\n\`/random\`: Gives a random value form the given list\n\`Arguments:\`\n> \`random:\` Random list **Must be seperated by commas**`
+// Convert EmbedBuilder object to JSON format
+const embedJson = exampleEmbed.toJSON();
 
-
-
-    return{
-        statusCode: 200,
-        body: JSON.stringify({
-            type:4,
-            data:{content:userMessage},
-        }),
-    };
+return {
+    statusCode: 200,
+    body: JSON.stringify({
+        type: 4,
+        data: {
+            embeds: [embedJson],
+        },
+    }),
 };
+}
+
---
+++ _pokemonTest.js
@@ -0,0 +1,60 @@
+const {EmbedBuilder} = require('discord.js')
+module.exports = async(body)=>{
+  const fetch = (await import('node-fetch')).default;
+  const randomPokemon = Math.floor(Math.random()*1025)+1;
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}/`);
+  const data = await response.json();
+ 
+  const pokemonName = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+  let types = data.types
+    .map(typeInfo => typeInfo.type.name.charAt(0).toUpperCase() + typeInfo.type.name.slice(1))
+    .join(" and ");
+
+    const speciesResponse = await fetch(data.species.url);
+    if (!speciesResponse.ok) throw new Error(`Failed to fetch Pokémon species: ${speciesResponse.status}`);
+
+    const speciesData = await speciesResponse.json();
+    const color = speciesData.color.name; 
+
+    const userId = body.member.user.id;
+
+    const pokemonColors = {
+        "black": 0x323232,
+        "blue": 0x3B9CFF,
+        "brown": 0xA56D4F,
+        "gray": 0x8B8B8B,
+        "green": 0x1DBC60,
+        "pink": 0xFF91A4,
+        "purple": 0xA63DE3,
+        "red": 0xE24242,
+        "white": 0xEDEDED,
+        "yellow": 0xF9D030
+    };
+
+    const pokeEmbed = new EmbedBuilder()
+    .setColor(pokemonColors[color] || "0x000000")
+    .setTitle(`A Wild ${pokemonName} Appeared`) 
+    .setURL(`https://www.pokemon.com/us/pokedex/${pokemonName.toLowerCase()}/`)
+    .setAuthor({name: "Yoshingo",iconURL: 'https://i.imgur.com/IlcxbcC.jpeg'})
+    .addFields(
+        {name: "Type", value: types},
+        {name: "Level", value:"100"}
+    )
+    .setImage('https://i.imgur.com/IlcxbcC.jpeg')
+    .setTimestamp()
+    .setFooter({text:"Developed by @Yoshingo", iconURL: 'https://i.imgur.com/IlcxbcC.jpeg'})
+
+
+    const embedJson = pokeEmbed.toJSON();
+    
+return {
+    statusCode: 200,
+    body: JSON.stringify({
+        type: 4,
+        data: {
+            embeds:[embedJson],
+        },
+    }),
+};
+}
+
